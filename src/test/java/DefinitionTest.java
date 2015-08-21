2062bdac3c143a942c0a9fd19266ef8e1fdbf608
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-21 08:36:44 -0700

    initial file structure set up

---
+++ src/test/java/DefinitionTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
+
+public class DefinitionTest {
+
+  @Rule
+   public ClearRule clearRule = new ClearRule();
+
+  // @Test
+  // public void definition_instantiatesCorrectly_true() {
+  //   Definition myDefinition = new Definition("booger");
+  //   assertEquals(true, myDefinition instanceof Definition);
+  // }
+
+}
---
+++ src/test/java/WordTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
+
+public class WordTest {
+
+  @Rule
+   public ClearRule clearRule = new ClearRule();
+
+  // @Test
+  // public void word_instantiatesCorrectly_true() {
+  //   Word myWord = new Word("booger");
+  //   assertEquals(true, myWord instanceof Word);
+  // }
+
+}

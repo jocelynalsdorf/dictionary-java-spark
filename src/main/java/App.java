8a01a56f2a689740d64269d1658f16c26070be7a
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-21 09:34:46 -0700

    adds route for index, new word form, success page; web application storing and showing as expected first integration test for home route passing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,30 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
-}
-}
+		
+		get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+   	}, new VelocityTemplateEngine());
+
+		get("/words/new", (request, response) -> {
+     	HashMap<String, Object> model = new HashMap<String, Object>();
+     	model.put("template", "templates/word-form.vtl");
+     	return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+		post("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Word newWord = new Word(name);
+      model.put("word", newWord);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+	}//end of main
+}//end of appclass
